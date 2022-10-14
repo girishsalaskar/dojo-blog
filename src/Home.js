9bc14d4a785e75cde27bf0cb5d745ed869b38343
Girish Salaskar <girishsalaskar812@gmail.com>
2022-10-14 19:14:15 +0530

    Fetch data from the server
    Use the fetch method and update the blogs data.

--- src/Home.js
+++ src/Home.js
@@ -2,13 +2,7 @@ import React, {useState, useEffect} from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ]);
-
-    const [name, setName] = useState('mario');
+    const [blogs, setBlogs] = useState(null);
 
     const handleDelete = (id) => {
         const newBlogs = blogs.filter( blog => blog.id!==id );
@@ -16,13 +10,18 @@ const Home = () => {
     }
 
     useEffect(() => {
-        console.log('useEffect called!', name);
-    }, [name]);
+        fetch('http://localhost:8000/blogs')
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                setBlogs(data);
+            })
+    }, []);
 
     return (
         <div className="home">
-            { name } - <button onClick={() => setName('luigi')}>Change</button>
-            <BlogList blogs={blogs} title={'All blogs'} handleDelete={handleDelete} />
+            { blogs && <BlogList blogs={blogs} title={'All blogs'} handleDelete={handleDelete} /> }
         </div>
     );
 }
