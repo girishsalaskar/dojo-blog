2e9e4d378eea3997d7f65badc2faa821374070e7
Girish Salaskar <girishsalaskar812@gmail.com>
2022-10-14 18:00:14 +0530

    Properties #1

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,8 @@
 import React, {useState} from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
+    const [blogs] = useState([
         { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
@@ -9,13 +10,7 @@ const Home = () => {
 
     return (
         <div className="home">
-            { blogs.map((blog) => {
-                return (<div className="blog-preview" key={blog.id}>
-                    <h2>{ blog.title }</h2>
-                    <p>Written by { blog.author }</p>
-
-                </div>);
-            }) }
+            <BlogList blogs={blogs} title={'All blogs'} />
         </div>
     );
 }
