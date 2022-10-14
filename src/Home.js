943f5a80652f5a09b67baa58cfd5887b504bca81
Girish Salaskar <girishsalaskar812@gmail.com>
2022-10-14 16:50:34 +0530

    Click Event

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,17 @@
 import React from "react";
 
 const Home = () => {
+    const clickMeHandler = () => {
+        console.log('Hello world!');
+    }
+    const clickMeAgainHandler = (name) => {
+        console.log('Hello ' + name);
+    }
     return (
         <div className="home">
             <h2>Home page</h2>
+            <button onClick={clickMeHandler}>Click Me</button>
+            <button onClick={()=>{clickMeAgainHandler('Iron Man')}}>Click Me Again</button>
         </div>
     );
 }
