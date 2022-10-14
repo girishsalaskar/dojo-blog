97d59a0b63e3dfdb87b000963410fd30c36042ca
Girish Salaskar <girishsalaskar812@gmail.com>
2022-10-14 16:42:13 +0530

    Lesson#6

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar />
         <div className="content">
-          <h1>App Component</h1>
+          <Home />
         </div>
       </div>
     );
