0b7b5240df906a8627329b24515086da6a3e0749
Girish Salaskar <girishsalaskar812@gmail.com>
2022-10-14 18:00:59 +0530

    Properties #2

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-const BlogList = (props) => {
-    const blogs = props.blogs;
-    const title = props.title;
+const BlogList = ({blogs, title}) => {
+    // const blogs = props.blogs;
+    // const title = props.title;
     return ( 
         <div className="blog-list">
             <h2>{title}</h2>
