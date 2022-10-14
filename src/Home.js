a9eb8be67811aeb4e118723dbab1d5563b74cda2
Girish Salaskar <girishsalaskar812@gmail.com>
2022-10-14 16:52:58 +0530

    Click event - Event handler

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,17 @@
 import React from "react";
 
 const Home = () => {
-    const clickMeHandler = () => {
-        console.log('Hello world!');
+    const clickMeHandler = (e) => {
+        console.log('Hello world!', e);
     }
-    const clickMeAgainHandler = (name) => {
-        console.log('Hello ' + name);
+    const clickMeAgainHandler = (name, e) => {
+        console.log('Hello ' + name, e);
     }
     return (
         <div className="home">
             <h2>Home page</h2>
             <button onClick={clickMeHandler}>Click Me</button>
-            <button onClick={()=>{clickMeAgainHandler('Iron Man')}}>Click Me Again</button>
+            <button onClick={(e)=>{clickMeAgainHandler('Iron Man', e)}}>Click Me Again</button>
         </div>
     );
 }
