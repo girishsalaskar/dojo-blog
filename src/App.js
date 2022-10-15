9ca4cfbeb016725f99dc4ad1f0fe20944caa29a4
Girish Salaskar <girishsalaskar812@gmail.com>
2022-10-15 14:10:39 +0530

    React Routing - Basic

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
 import Navbar from './Navbar';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <div className="content">
-          <Home />
+      <Router>
+        <div className="App">
+          <Navbar />
+          <div className="content">
+            <Switch>
+              <Route path='/'>
+                <Home />
+              </Route>
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
